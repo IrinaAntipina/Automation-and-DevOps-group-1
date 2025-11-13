9f87fac7a23e562aa53a049b44487528fdd08ae7
IrinaAntipina <iramaskaykina@gmail.com>
2025-11-13 17:15:49 +0100

    broken test

--- tests/test_not_null.py
+++ tests/test_not_null.py
@@ -3,5 +3,8 @@ from weather_api_call import fetch_weather_data
 def test_fetch_weather_data():
     df = fetch_weather_data(59.33, 18.07)
     
-    assert df is not None  
-    assert not df.empty
+    #assert df is not None  
+  #  assert not df.empty
+
+    assert df is None
+
